7f51f066b6b66ce98d7b1f2a5ff46677aed56675
Amar <augustine2903a@gmail.com>
2024-10-18 15:02:59 -0400

    initial all folders

---
+++ config/dbConnect.js
@@ -0,0 +1,17 @@
+// require('dotenv').config();
+// const mongoose = require('mongoose');
+
+// const connectDB = () => {
+//   // let DB_URI = '';
+
+//   // if (process.env.NODE_ENV === 'DEVELOPMENT') DB_URI = process.env.DB_LOCAL_URI;
+//   // if (process.env.NODE_ENV === 'PRODUCTION') DB_URI = process.env.DB_URI;
+
+//   mongoose.connect('mongodb://127.0.0.1:27017/super_market').then((con) => {
+//     console.log(`MongoDB Database connected with HOST: ${con?.connection?.host}`);
+//   });
+// };
+
+// module.exports = {
+//   connectDB,
+// };
